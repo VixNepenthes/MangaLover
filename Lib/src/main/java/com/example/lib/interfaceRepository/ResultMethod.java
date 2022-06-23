3aecf1c3b854bdfe4dbe3f0e80f651c134c809ed
VixNepenthes <viht61220@gmail.com>
2022-06-24 00:22:06 +0700

    Destroy Lib

--- Lib/src/main/java/com/example/lib/interfaceRepository/ResultMethod.java
+++
@@ -1,9 +0,0 @@
-package com.example.lib.interfaceRepository;
-
-import com.example.lib.model.StoryModel;
-
-import java.util.List;
-
-public interface ResultMethod {
-    void onSuccess(List<StoryModel> modelList);
-}
